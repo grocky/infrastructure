be9f40f5f94e9b51bcd6430c09065f4fba9876f4
Rocky Gray <rocky.grayjr@gmail.com>
2020-05-29 19:49:57 -0400

    don't stutter for remote state reverences

--- rockygray.com/interface.tf
+++ rockygray.com/interface.tf
@@ -6,6 +6,18 @@ variable "region" {
   default = "us-east-1"
 }
 
-output "outputs" {
-  value = module.root.outputs
+output "root_zone_id" {
+  value = module.root.outputs.root_zone_id
+}
+
+output "nameservers" {
+  value = module.root.outputs.nameservers
+}
+
+output "certificate_arn" {
+  value = module.root.outputs.certificate_arn
+}
+
+output "cloudfront_id" {
+  value = module.root.outputs.cloudfront_id
 }
