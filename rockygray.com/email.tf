c23f2c979289b0195e6d00e5bf7a69877c82c5bf
Rocky Gray <rocky.grayjr@gmail.com>
2020-05-30 11:24:55 -0400

    add ses domain DKIM for authenticating email

--- rockygray.com/email.tf
+++ rockygray.com/email.tf
@@ -1,3 +1,4 @@
+# SES Domain Identity
 resource "aws_ses_domain_identity" "rockygray" {
   domain = var.domain
 }
@@ -14,4 +15,22 @@ resource "aws_ses_domain_identity_verification" "rockygray_verification" {
   domain = aws_ses_domain_identity.rockygray.id
 
   depends_on = [aws_route53_record.rockygray_amazonses_verification_record]
+}
+
+# SES DKIM
+resource "aws_ses_domain_dkim" "rockygray" {
+  domain = "${aws_ses_domain_identity.rockygray.domain}"
+}
+
+resource "aws_route53_record" "rockygray_amazonses_dkim_record" {
+  count   = 3
+  zone_id = module.root.outputs.root_zone_id
+  name = format(
+    "%s._domainkey.%s",
+    element(aws_ses_domain_dkim.rockygray.dkim_tokens, count.index),
+    aws_ses_domain_identity.rockygray.domain
+  )
+  type    = "CNAME"
+  ttl     = "600"
+  records = ["${element(aws_ses_domain_dkim.rockygray.dkim_tokens, count.index)}.dkim.amazonses.com"]
 }
