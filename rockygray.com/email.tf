4477b2dccfb6a476211cd130746cc1f6998e8627
Rocky Gray <rocky.grayjr@gmail.com>
2020-05-30 09:59:47 -0400

    fix ses verification record

--- rockygray.com/email.tf
+++ rockygray.com/email.tf
@@ -2,9 +2,9 @@ resource "aws_ses_domain_identity" "rockygray" {
   domain = var.domain
 }
 
-resource "aws_route53_record" "example_amazonses_verification_record" {
+resource "aws_route53_record" "rockygray_amazonses_verification_record" {
   zone_id = module.root.outputs.root_zone_id
-  name    = "_amazonses.example.com"
+  name    = "_amazonses.${aws_ses_domain_identity.rockygray.id}"
   type    = "TXT"
   ttl     = "600"
   records = [aws_ses_domain_identity.rockygray.verification_token]
