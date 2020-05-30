5c74642a4697332fdad61a8c3ad8c7caa9c1410d
Rocky Gray <rocky.grayjr@gmail.com>
2020-05-30 10:25:18 -0400

    Add the SES domain verification

--- rockygray.com/email.tf
+++ rockygray.com/email.tf
@@ -9,3 +9,9 @@ resource "aws_route53_record" "rockygray_amazonses_verification_record" {
   ttl     = "600"
   records = [aws_ses_domain_identity.rockygray.verification_token]
 }
+
+resource "aws_ses_domain_identity_verification" "rockygray_verification" {
+  domain = aws_ses_domain_identity.rockygray.id
+
+  depends_on = [aws_route53_record.rockygray_amazonses_verification_record]
+}
