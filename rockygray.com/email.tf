7537aa8934f462f1665d4b2ce83f46f0069003b8
Rocky Gray <rocky.grayjr@gmail.com>
2020-05-30 12:15:40 -0400

    create receive MX record for apex domain

--- rockygray.com/email.tf
+++ rockygray.com/email.tf
@@ -35,18 +35,10 @@ resource "aws_route53_record" "rockygray_amazonses_dkim_record" {
   records = ["${element(aws_ses_domain_dkim.rockygray.dkim_tokens, count.index)}.dkim.amazonses.com"]
 }
 
-# Mail FROM
+# Mail Send and Receive
 resource "aws_ses_domain_mail_from" "rockygray" {
   domain           = aws_ses_domain_identity.rockygray.domain
   mail_from_domain = "mail.${aws_ses_domain_identity.rockygray.domain}"
-}
-
-resource "aws_route53_record" "rockygray_ses_domain_mail_from_mx" {
-  zone_id = module.root.outputs.root_zone_id
-  name    = aws_ses_domain_mail_from.rockygray.mail_from_domain
-  type    = "MX"
-  ttl     = "600"
-  records = ["10 feedback-smtp.${var.region}.amazonses.com"]
 }
 
 locals {
@@ -63,4 +55,20 @@ resource "aws_route53_record" "rockygray_ses_domain_spf_txt" {
   type    = "TXT"
   ttl     = "600"
   records = ["v=spf1 include:amazonses.com -all"]
+}
+
+resource "aws_route53_record" "rockygray_ses_domain_mail_from_mx" {
+  zone_id = module.root.outputs.root_zone_id
+  name    = aws_ses_domain_mail_from.rockygray.mail_from_domain
+  type    = "MX"
+  ttl     = "600"
+  records = ["10 feedback-smtp.${var.region}.amazonses.com"]
+}
+
+resource "aws_route53_record" "rockygray_ses_domain_receive_mx" {
+  zone_id = module.root.outputs.root_zone_id
+  name    = aws_ses_domain_identity.rockygray.domain
+  type    = "MX"
+  ttl     = "600"
+  records = ["10 inbound-smtp.${var.region}.amazonses.com"]
 }
