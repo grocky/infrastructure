9958ec98aaf5d8f79dd6c8e7a79c24eddc05e68d
Rocky Gray <rocky.grayjr@gmail.com>
2020-05-30 11:40:56 -0400

    Add Mail FROM domain

--- rockygray.com/email.tf
+++ rockygray.com/email.tf
@@ -34,3 +34,25 @@ resource "aws_route53_record" "rockygray_amazonses_dkim_record" {
   ttl     = "600"
   records = ["${element(aws_ses_domain_dkim.rockygray.dkim_tokens, count.index)}.dkim.amazonses.com"]
 }
+
+# Mail FROM
+resource "aws_ses_domain_mail_from" "rockygray" {
+  domain           = aws_ses_domain_identity.rockygray.domain
+  mail_from_domain = "mail.${aws_ses_domain_identity.rockygray.domain}"
+}
+
+resource "aws_route53_record" "rockygray_ses_domain_mail_from_mx" {
+  zone_id = module.root.outputs.root_zone_id
+  name    = aws_ses_domain_mail_from.rockygray.mail_from_domain
+  type    = "MX"
+  ttl     = "600"
+  records = ["10 feedback-smtp.${var.region}.amazonses.com"]
+}
+
+resource "aws_route53_record" "rockygray_ses_domain_mail_from_txt" {
+  zone_id = module.root.outputs.root_zone_id
+  name    = aws_ses_domain_mail_from.rockygray.mail_from_domain
+  type    = "TXT"
+  ttl     = "600"
+  records = ["v=spf1 include:amazonses.com -all"]
+}
