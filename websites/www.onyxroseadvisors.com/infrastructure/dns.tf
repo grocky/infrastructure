6dfb487ff57efce95b6854f0f5c8707c7fa8e7ce
Rocky Gray <rocky.grayjr@gmail.com>
2024-06-07 16:48:56 -0400

    add email records for onyxroseadvisors.com

--- websites/www.onyxroseadvisors.com/infrastructure/dns.tf
+++ websites/www.onyxroseadvisors.com/infrastructure/dns.tf
@@ -9,3 +9,24 @@ resource "aws_route53_record" "www" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "mx" {
+  zone_id = data.terraform_remote_state.root.outputs.root_domain.root_zone_id
+  name    = ""
+  type    = "MX"
+  records = [
+    "10 fwd1.porkbun.com",
+    "20 fwd2.porkbun.com",
+  ]
+  ttl = 3600
+}
+
+resource "aws_route53_record" "txt" {
+  zone_id = data.terraform_remote_state.root.outputs.root_domain.root_zone_id
+  name    = ""
+  type    = "TXT"
+  records = [
+    "v=spf1 mx include:_spf.porkbun.com ~all",
+  ]
+  ttl = 3600
+}
